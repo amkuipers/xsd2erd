0c8e47413433012039169d7fe63b725a9883b31e
AMKuipers <amkuipers@gmail.com>
2024-07-06 18:23:17 +0200

    Create xsd2erd.py

---
+++ xsd2erd.py
@@ -0,0 +1,54 @@
+import xmlschema
+
+def parse_xsd(xsd_path):
+    schema = xmlschema.XMLSchema(xsd_path)
+    entities = {}
+    relationships = []
+
+    for stype in schema.types.values():
+        if stype.is_complex():
+            attributes = []
+            for subelement in stype.content.iter_elements():
+                subelement_type = subelement.type.local_name if not subelement.type.is_complex() else subelement.name
+                attributes.append(f"{subelement_type} {subelement.name}")
+                # Detect potential relationships from nested elements
+                if subelement.type.is_complex():
+                    #relationships.append((stype.name, subelement.name))
+                    relationships.append((stype.name, subelement.type.name))
+            entities[stype.name] = attributes
+
+    for element in schema.elements.values():
+        if element.type.is_complex():
+            attributes = []
+            for subelement in element.type.content.iter_elements():
+                subelement_type = subelement.type.local_name if not subelement.type.is_complex() else subelement.name
+                attributes.append(f"{subelement_type} {subelement.name}")
+                # Detect potential relationships from nested elements
+                if subelement.type.is_complex():
+                    #relationships.append((element.name, subelement.name))
+                    relationships.append((element.name, subelement.type.name))
+            entities[element.name] = attributes
+
+    return entities, relationships
+
+def generate_mermaid_erdiagram(entities, relationships):
+    er_diagram = "erDiagram\n"
+    for entity, attributes in entities.items():
+        er_diagram += f"    {entity} {{\n"
+        for attribute in attributes:
+            er_diagram += f"        {attribute}\n"
+        er_diagram += f"    }}\n"
+
+    # Add relationships
+    for (parent, child) in relationships:
+        er_diagram += f"    {parent} ||--o{{ {child} : contains\n"
+
+    return er_diagram
+
+xsd_path = 'books.xsd' 
+entities, relationships = parse_xsd(xsd_path)
+mermaid_code = generate_mermaid_erdiagram(entities, relationships)
+
+print(mermaid_code)
+
+            
